cf2ebe1b9aa3bafc79e8bc07fa1a447ffb3c5f98
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-03-25 20:56:48 +0900

    Update 60057.py

--- kakao/60057.py
+++ kakao/60057.py
@@ -1,15 +1,18 @@
 def solution(s):
     l = len(s)
-    counts =[] 
-    for i in range(1,l//2+1): #i개 단위일 때
-        count=0
-        for a in range(0,l,i):
-            if (a+2*i >= l):
-                break
-            if (s[a:a+i]==s[a+i:a+2*i]):
-                count+=1
-        counts.append(count)
-    print(counts)
-    return max(counts)
+    count = []
+    for i in range(1,l//2+1):
+        n = l
+        for j in range(0,l-2*i+1, i):
+            dup = False
+            while(s[j:j+i]== s[j+i:j+2*i]):
+                dup = True
+                n -= i
+                j+=i
+            if(dup):
+                n+=1
+        count.append(n)
+    print(count)
+    return min(count)
 
-solution("ababcdcdababcdcd")
+solution("abcabcabcabcdededededede")
