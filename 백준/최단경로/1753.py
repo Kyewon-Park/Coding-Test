09d40a79d32f26994bc8eda7c4fd4e6966044a80
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-17 15:16:58 +0900

    Create 1753.py

---
+++ 백준/최단경로/1753.py
@@ -0,0 +1,33 @@
+import heapq
+import sys
+input=sys.stdin.readline
+INF=int(1e9)
+
+v,e = map(int, input().split())
+start=int(input())
+graph=[[] for i in range(v+1)]
+for _ in range(e):
+    a,b,c = map(int, input().split())
+    graph[a].append((b,c))
+distance=[INF]*(v+1)
+
+def dijkstra(start):
+    q=[]
+    heapq.heappush(q,(0,start)) #dist,now
+    distance[start]=0
+    while q:
+        dist,now=heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for i in graph[now]:
+            cost=dist+i[1]
+            if distance[i[0]]>cost:
+                distance[i[0]]=cost
+                heapq.heappush(q,(cost,i[0]))
+
+dijkstra(start)
+for i in distance[1:]:
+    if i==INF:
+        print("INF")
+        continue
+    print(i)
