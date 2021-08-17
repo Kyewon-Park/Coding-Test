6b91ce1c30e9e1d3304e3b3ff296452af331372a
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-17 15:16:40 +0900

    Create 11726.py

---
+++ 백준/다이나믹/11726.py
@@ -0,0 +1,17 @@
+#2*n타일링
+# n=int(input())
+# d= [0]*(n+1)
+# d[1]=1
+# d[2]=2
+# if n<=2:
+#     print(d[n])
+# else:
+#     for i in range(3,n+1):
+#         d[i]=d[i-1]+d[i-2]
+#     print(d[n]%10007)
+
+n=int(input())
+s = [0, 1, 2]
+for i in range(3, 1001):
+  s.append(s[i - 2] + s[i - 1])
+print(s[n] % 10007)
