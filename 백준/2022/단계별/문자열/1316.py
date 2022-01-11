4d1091efa1ac08092ebc1a4acea722777f93927d
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-01-11 19:39:20 +0900

    Update 1316.py

--- 백준/2022/단계별/문자열/1316.py
+++ 백준/2022/단계별/문자열/1316.py
@@ -1,3 +1,21 @@
-#아스키
-
-print(ord(input()))
+#그룹 단어 체커
+n = int(input())
+ans = 0
+for i in range(n):
+    s= input()
+    prev=''
+    ss = set()
+    isPass = True
+    for c in s:
+        if c in ss:
+            if c == prev:
+                continue
+            else:
+                isPass = False
+                break
+        else:
+            ss.add(c)
+            prev = c
+    if isPass:
+        ans += 1
+print(ans)
