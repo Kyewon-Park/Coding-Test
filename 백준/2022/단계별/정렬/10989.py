84e94849d3be2b298445bbcba1a7f74a037d7a78
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-01-22 23:20:58 +0900

    Create 10989.py

---
+++ 백준/2022/단계별/정렬/10989.py
@@ -0,0 +1,12 @@
+#수 정렬하기3
+
+import sys
+input = sys.stdin.readline
+n=int(input())
+l = [0]*10001
+for _ in range(n):
+    l[int(input())]+=1
+for i in range(1,10001):
+    if l[i]!=0:
+        for _ in range(l[i]):
+            print(i)
