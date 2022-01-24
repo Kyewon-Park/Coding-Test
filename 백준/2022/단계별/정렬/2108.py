e391c2b4080eaae01101a3f5e9654862a7f47508
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-01-24 16:31:14 +0900

    Update 2108.py

--- 백준/2022/단계별/정렬/2108.py
+++ 백준/2022/단계별/정렬/2108.py
@@ -1,18 +1,17 @@
 #통계학
+from collections import Counter
 import sys
 input = sys.stdin.readline
 n=int(input())
-l = [0]*4001
-lm = [0]*4001
+l=[]
 for _ in range(n):
-    inp =int(input())
-    if inp<0: 
-        lm[abs(inp)]+=1
-    else:
-        l[inp]+=1
-mean = int((sum(l) - sum(lm))/n)
-print(f"{mean}")
-for i in range(1,10001):
-    if l[i]!=0:
-        for _ in range(l[i]):
-            print(i)
+    l.append(int(input()))
+l.sort()
+print(round(sum(l)/n))
+print(l[n//2])
+cl = Counter(l).most_common(2)
+if n>1 and cl[0][1] == cl[1][1]:
+    print(cl[1][0])
+else:
+    print(cl[0][0])
+print(l[-1]-l[0])
