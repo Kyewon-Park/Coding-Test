1b86bc8bf8508bf1e9b4bd7e59e3ed150c2ad142
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-01-27 22:55:11 +0900

    Create 1149.py

---
+++ 백준/2022/단계별/동적계획법1/1149.py
@@ -0,0 +1,11 @@
+#RGB거리
+#!!!!!!!!!!!!!
+import sys
+input = sys.stdin.readline
+n = int(input().strip())
+l = [list(map(int,input().strip().split())) for _ in range(n)]
+for i in range(1,n):
+    l[i][0] = min(l[i-1][1],l[i-1][2]) + l[i][0]
+    l[i][1] = min(l[i-1][0],l[i-1][2]) + l[i][1]
+    l[i][2] = min(l[i-1][0],l[i-1][1]) + l[i][2]
+print(min(l[n-1]))
