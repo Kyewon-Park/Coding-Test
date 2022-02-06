cd6f8d401854df52f3304646fbc9f24a5f22d47b
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-02-06 23:36:18 +0900

    Update 11053.py

--- 백준/2022/단계별/동적계획법1/11053.py
+++ 백준/2022/단계별/동적계획법1/11053.py
@@ -1,17 +1,11 @@
 # 가장 긴 증가하는 부분수열
+# !!!!!!!!!!!!!
 input()
 l = list(map(int, input().split()))
-order=0
-endN=[0]
-countN=[0]
-for i in l:
-    for j in range(order+1):
-        if i > endN[j]:
-            countN[j]+=1
-            endN[j]=i
-        else:
-            order+=1
-            endN.append(i)
-            countN.append(0)
-            break
-print(max(countN))
+n = len(l)
+dp=[1]*n
+for i in range(1,n):
+    for j in range(i):
+        if l[i] > l[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
