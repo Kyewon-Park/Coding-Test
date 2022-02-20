91a291029c3fbf42ffb29863a0093383c853a2f1
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-02-21 02:49:29 +0900

    Update 9012.py

--- 백준/2022/단계별/스택/9012.py
+++ 백준/2022/단계별/스택/9012.py
@@ -4,12 +4,20 @@ import sys
 input = sys.stdin.readline
 N = int(input().strip())
 for _ in range(N):
-    l = list(input().strip().split(')('))
-    print(l)
-    # up=down=0
-    # for i in l:
-    #     if i == '(':
-    #         up+=1
-    #     else:
-    #         down+=1
-        
+    stk=[]
+    l = list(input().strip())
+    n = len(l)
+    for i in range(n):
+        stk.append(l[i])
+        if l[i]=='(':
+            continue
+        elif len(stk)>1 and l[i]==')':
+            stk.pop()
+            stk.pop()
+        else:
+            break
+    if len(stk)==0:
+        print('YES')
+    else:
+        print('NO')
+
