0ee4de811125576d88fd9680c418dfd72b676e1a
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-02-28 21:09:53 +0900

    Update 11866.py

--- 백준/2022/단계별/큐/11866.py
+++ 백준/2022/단계별/큐/11866.py
@@ -1,2 +1,22 @@
 # 요세푸스
+n, k = map(int, input().split())
+li = [i for i in range(n)]
+ans=[]
+for _ in range(n):
+    while li[k]==False:
+        k+=1
+        if k>=n:
+            k-=n
+    ans.append(li[k])
+    li[k]=False
+    x=0
+    while x!= 3:
+        k+=1
+        if k>=n:
+            k-=n
+        if li[k]==True:
+            x+=1
+           
+    
 
+print(f"<{','.join(ans)}")
