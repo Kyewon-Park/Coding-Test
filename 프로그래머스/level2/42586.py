595eb84928c5b6383d8942cb086b514ab873ac5b
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2022-04-21 22:02:32 +0900

    Update 42586.py

--- 프로그래머스/level2/42586.py
+++ 프로그래머스/level2/42586.py
@@ -1,38 +1,21 @@
-import timeit
-
+#기능개발
+import math
 def solution(progresses, speeds):
-    days = []
-    for p,s in zip(progresses,speeds):
-        quo = int((100-p)/s) #70/30 = 2
-        if((100-p)%s!=0):
-            quo+=1
-        days.append(quo)
-
-    for i in range(len(days)-1):
-        if days[i+1] < days[i]:
-            days[i+1] = days[i]
-    
-    return counts(days)
-
-def counts():
-    numbers = [5, 10,10,10, 15,15, 8,8,8,8]
-    answer=[]
-    now = numbers[0]
+    left = []
+    for i in range(len(progresses)):
+        left.append(math.ceil((100-progresses[i])/speeds[i]))
+    print(left)
+    result=[]
+    past = left[0]
     count=1
-    done=False
-    for n in numbers[1:]:
-        if now == n:
-            count+=1
-            done=False
-        else:
-            answer.append(count)
-            count=1
-            now=n
-            done=True
-    if(not done):
-        answer.append(count)
-    if(done):
-        answer.append(1)
-    return answer
-
-print("'Manual' method average time:", timeit.timeit(counts, number=10000))
+    if len(left) > 1:
+        for now in left[1:]: 
+            if past<now:
+                result.append(count)
+                count=1
+                past = now
+            else:
+                count+=1
+    result.append(count)
+        
+    return result
